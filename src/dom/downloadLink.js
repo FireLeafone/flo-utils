d746b02527b3eb58d7e8fcceb81163fb595843f6
NARUTOne <zhong.wu@100credit.com>
2019-01-22 19:47:43 +0800

    'version'

--- src/dom/downloadLink.js
+++ src/dom/downloadLink.js
@@ -6,13 +6,7 @@
  * @description 模拟下载
  */
 
-const URL_REG = /^((https?|ftp|file):\/\/)?([\da-z\\.-]+)\.([a-z\\.]{2,6})([\\/\w \\.-]*)*\/?$/;
-
 const downloadLink = (url = '', filename = 'file') => {
-  if (!URL_REG.test(url)) {
-    new Error('url is error');
-    return;
-  }
   const a = document.createElement('a');
   a.href = url;
   a.download = filename;

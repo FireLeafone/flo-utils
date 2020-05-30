7d970191ab867ce1b75a32516a995e631752991a
NARUTOne <wznaruto326@163.com>
2020-05-30 14:11:21 +0800

    fire-tools

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,7 @@ module.exports = function (api) {
   ];
 
   const plugins = [
+    'lodash',
     [
       '@babel/plugin-transform-runtime',
       {
--- fire-tools.config.js
+++ fire-tools.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  asePath: "src", // 源代码基本路径
+  basePath: "src", // 源代码基本路径
   buildPath: "dist", // 打包路径
   webpackConfigPath: "./script/webpack.pro.config.js", // webpack 配置路径
   lessPath: [], // 编译的less相对src路径，正则形式 /(\/|\\)style(\/|\\)index\.less$/
